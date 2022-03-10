f5580efd780432d872c3b39ff048179dbf99962c
JhonnyVicente1019 <jhonnyvicentecolque@gmail.com>
2022-03-10 16:02:35 -0300

    Creación

---
+++ RepasoPersona.Test/PersonaTest.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using RepasoPersona.Core;
+
+namespace RepasoPersona.Test
+{
+    public class PersonaTest
+    {
+        public Persona Pepito{get; set;}
+        [Fact]
+        public void Contructor()
+        {
+            
+            {
+                Pepito = new Persona("Pepito", "Gomez", 0);
+                Assert.Equal("Pepito", Pepito.Nombre);
+                Assert.Equal("Gomez", Pepito.Apellido);
+                Assert.Equal(0, Pepito.Efectivo);
+            }
+        }
+    }
+}
